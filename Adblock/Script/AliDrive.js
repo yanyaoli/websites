6ed1a4dd38755f6a80f339ab39e5c8350d6485da
Yoo <120553430+eoooy@users.noreply.github.com>
2023-01-02 17:39:09 +0800

    Delete

--- Adblock/Script/AliDrive.js
+++
@@ -1,5 +0,0 @@
-let obj = JSON.parse($response.body);
-if (obj.result) {
-    obj.result = Object.values(obj.result).filter(item => (item["appCode"]=="file" || item["appCode"]=="video"));
-}
-$done({ body: JSON.stringify(obj) });
